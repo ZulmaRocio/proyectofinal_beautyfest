645f55c940f1a02021ded6d5e0bd0d67d320b194
mahperez <miguel.huertas@dale.com.co>
2022-06-19 22:37:51 -0500

    servicio producto

--- Front_beatifulFest/beatiful_fest/src/app/services/producto.service.ts
+++ Front_beatifulFest/beatiful_fest/src/app/services/producto.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  url ='http://localhost:4000/api/producto/'
+
+  constructor(private http: HttpClient ) { }
+
+  //consulta de todos los productos
+  getProductos(): Observable<any> {
+    return this.http.get(this.url + '/obtenerProducto' )
+  }
+
+
 }
